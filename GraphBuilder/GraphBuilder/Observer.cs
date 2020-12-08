83222f19ebd33bec04722362ef416ca0e3ecc007
Zachery <zacherysmith115@gmail.com>
2020-12-07 19:39:57 -0500

    switching branches

---
+++ GraphBuilder/GraphBuilder/Observer.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Forms;
+
+namespace GraphBuilder.Graphing
+{
+    public interface ObserverIF
+    {
+
+        void notify(Panel p);
+    }
+
+
+    public class Notifier
+    {
+        int[] x_values;
+        Point[] points;
+
+        public Notifier(int x)
+        {
+            x_values = new int[x];
+            points = new Point[x];
+        }
+
+
+        public void notify(Panel p, int x)
+        {
+
+        }
+    }
+}
